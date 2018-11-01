79b72e708749d275e6484b5e8b191cdec37e747b
BERTHA QUETZALY MARQUEZ CARO <12170631@itculiacan.edu.mx>
2018-11-01 09:59:42 -0700

    Calculadora parte 2

--- calculadora/js/calculadora.js
+++ calculadora/js/calculadora.js
@@ -1,8 +1,44 @@
+window.onload = function(){
+	var colorAmarillo = function(){
+			this.style.background = "yellow"
+}
+var colorBlanco = function(){
+			this.style.background = "white"
+}
 
-var colorAmarillo = function(x){
-			x.style.background = "yellow"
+var operando1 = document.getElementById("operando1")
+operando1.addEventListener("focus", colorAmarillo)
+operando1.addEventListener("focusout", colorBlanco)
 }
-var colorBlanco = function(x){
-			x.style.background = "white"
+
+var numeros = function(){
+	var valor=this.value;
+	if (operador=="") {
+		var valorInput=document.getElement("operando1").value;
+		if (valorInput == "0") {
+			document.getElementById("operando1").value="";
+		}
+		document.getElementById("operando1").value+=valor;
+	}
+	else{
+		if (operador=="") {
+		var valorInput=document.getElement("operando2").value;
+		if (valorInput == "0") {
+			document.getElementById("operando2").value="";
+		}
+		document.getElementById("operando2").value+=valor;
+	}
 }
+
+var uno=document.getElementById("uno")
+var dos=document.getElementById("dos")
+var uno=document.getElementById("uno")
+var uno=document.getElementById("uno")
+var uno=document.getElementById("uno")
+var uno=document.getElementById("uno")
+var uno=document.getElementById("uno")
+var uno=document.getElementById("uno")
+var uno=document.getElementById("uno")
+
+uno.addEventListener("click", numeros)
 
